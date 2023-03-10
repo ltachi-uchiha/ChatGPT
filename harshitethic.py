37e6278225282c58ce1cb8392b3487a040123c8d
ltachi-uchiha <127491173+ltachi-uchiha@users.noreply.github.com>
2023-03-10 14:49:15 +0500

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
